67f7277719cf95fcc3d73213f5478ca4475da2b8
Sarah Gebauer <sarah@blogonapaper.com>
2014-11-22 14:38:13 +0100

    Hangman p1
    I’m stuck a little bit

---
+++ 2014-11-17 hangman/hangman.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html>
+	<head>
+		<title>2014-11-17 Hangman</title>
+	</head>
+	<body>
+<?php
+	if (!isset($_GET["letter"]) && !isset($_GET["difficulty"])) : ?>		
+		<h1>Difficulty settings</h1>
+		<form method="post" action="?difficulty">
+			<input type="radio" name="diff" value="easy" /> Easy<br />
+			<input type="radio" name="diff" value="medium" /> Medium<br />
+			<input type="radio" name="diff" value="hard" /> Hard<br />
+			<input type="submit" value="Choose difficulty" />
+		</form>
+	<?php		
+	elseif (isset($_GET["difficulty"])) :
+		if (isset($_POST["diff"])) :
+			$min = 0;
+			$max;
+			switch ($_POST["diff"]) {
+				case "easy":
+					$min = 7;
+					$max = -1;
+					break;
+				case "medium":
+					$min = 5;
+					$max = 7;
+					break;
+				case "hard":
+					$min = 3;
+					$max = 5;
+					break;			
+				default:
+			}
+			if ($min == 0) : ?>
+			Please, <a href="hangman.php">try again</a>
+			<?php
+			else :
+			$handle = @fopen("hangman.txt", "r");
+			$list = [];
+			if ($handle) {
+				while (($buffer = fgets($handle, 4096)) !== false) {
+					$list[] = $buffer;
+					if ($min == 7) {
+						if ((strlen($buffer) > $max) && (strlen($buffer) > 7))
+							$max = strlen($buffer);
+					}
+				}
+				if (!feof($handle)) {
+			        echo "Error: unexpected fgets() fail\n";
+			    }
+			    fclose($handle);
+			}
+			$start = rand(0, count($list)-1);
+			$chosen = "";
+			$startedAgain = false;
+			for ($i = $start; $i<count($list); $i++) {
+				if ((strlen($list[$i]) >= $min) && (strlen($list[$i]) <= $max)) {
+					$chosen = $list[$i];
+					break;
+				}
+				if ($i == (count($list)-1)) {
+					$i = 1;
+					$startedAgain = true;	
+				}
+				if ($startedAgain && ($i == $start))
+					die("Wrong word list");
+			}
+						
+			endif;
+		else :?>
+		Please, <a href="hangman.php">try again</a>
+		<?php	
+		endif;
+	elseif (isset($_GET["letter"])) :
+	
+	else : ?>
+		Please, <a href="hangman.php">try again</a>
+	<?php	
+	endif;
+	
+	function printStuff()
+?>
+	</body>
+</html>
