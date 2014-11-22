ffceb49eb1b5f9cf08c823a26d6a63845eb49acb
Sarah Gebauer <sarah@blogonapaper.com>
2014-11-22 15:35:50 +0100

    pre-killing phase

--- 2014-11-17 hangman/hangman.php
+++ 2014-11-17 hangman/hangman.php
@@ -5,6 +5,10 @@
 	</head>
 	<body>
 <?php
+	define("WORD", 0);
+	define("LETTERS", 1);
+	define("ERRORS", 2);
+
 	if (!isset($_GET["letter"]) && !isset($_GET["difficulty"])) : ?>		
 		<h1>Difficulty settings</h1>
 		<form method="post" action="?difficulty">
@@ -34,49 +38,50 @@
 				default:
 			}
 			if ($min == 0) : ?>
-			Please, <a href="hangman.php">try again</a>
-			<?php
+				Please, <a href="hangman.php">try again</a>
+				<?php
 			else :
-			$handle = @fopen("hangman.txt", "r");
-			$list = [];
-			if ($handle) {
-				while (($buffer = fgets($handle, 4096)) !== false) {
-					$list[] = $buffer;
-					if ($min == 7) {
-						if ((strlen($buffer) > $max) && (strlen($buffer) > 7))
-							$max = strlen($buffer);
+				$handle = @fopen("hangman.txt", "r");
+				$list = [];
+				if ($handle) {
+					while (($buffer = fgets($handle, 4096)) !== false) {
+						$list[] = $buffer;
+						if ($min == 7) {
+							if ((strlen($buffer) > $max) && (strlen($buffer) > 7))
+								$max = strlen($buffer);
+						}
 					}
+					if (!feof($handle)) {
+				        echo "Error: unexpected fgets() fail\n";
+				    }
+			    	fclose($handle);
 				}
-				if (!feof($handle)) {
-			        echo "Error: unexpected fgets() fail\n";
-			    }
-			    fclose($handle);
-			}
-			$start = rand(0, count($list)-1);
-			$chosen = "";
-			$startedAgain = false;
-			for ($i = $start; $i<count($list); $i++) {
-				if ((strlen($list[$i]) >= $min) && (strlen($list[$i]) <= $max)) {
-					$chosen = $list[$i];
-					break;
+				$start = rand(0, count($list)-1);
+				$chosen = "";
+				$startedAgain = false;
+				for ($i = $start; $i<count($list); $i++) {
+					if ((strlen($list[$i]) >= $min) && (strlen($list[$i]) <= $max)) {
+						$chosen = $list[$i];
+						break;
+					}
+					if ($i == (count($list)-1)) {
+						$i = 1;
+						$startedAgain = true;	
+					}
+					if ($startedAgain && ($i == $start))
+						die("Wrong word list");
 				}
-				if ($i == (count($list)-1)) {
-					$i = 1;
-					$startedAgain = true;	
-				}
-				if ($startedAgain && ($i == $start))
-					die("Wrong word list");
-			}
-			$handle = fopen("temp", "w");
-			fwrite($handle, $chosen);
-			fclose($handle);			
+				$handle = fopen("temp", "w");
+				fwrite($handle, $chosen);
+				fclose($handle);			
+				printStuff();
 			endif;
 		else :?>
 		Please, <a href="hangman.php">try again</a>
 		<?php	
 		endif;
 	elseif (isset($_GET["letter"])) :
-	
+		printStuff();
 	else : ?>
 		Please, <a href="hangman.php">try again</a>
 	<?php	
@@ -95,9 +100,25 @@
 			    echo "Error: unexpected fgets() fail\n";
 			}
 			fclose($handle);
+		} 
+		if(strlen($letter) == 1) {
+			if (strpos($data[0], $letter))
+				$data[LETTERS] .= ",".$letter;
+		}
+		$toPrint;
+		if ((strpos($data[LETTERS], "=") == (strlen($data[LETTERS])-1)) || ($data[LETTERS] == null)) {
+			for ($i = 0; $i < strlen($data[WORD]); $i++) {
+				$toPrint .= "_ ";
+			}
 		}
 		//print graphics here
-		
+		echo $toPrint;
+		?>
+			<form action="hangman.php?letter" method="post">
+				<input type="text" name="letter" />
+				<input type="submit" value="Try letter" />
+			</form>
+		<?php
 	}
 ?>
 	</body>
